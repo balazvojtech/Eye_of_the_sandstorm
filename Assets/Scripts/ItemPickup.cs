f83c00f61d76955afd14f4aee906fd5de8f39b7c
Vojtech Baláž <vojtikbalaz@gmail.com>
2024-06-13 12:59:15 +0200

    removed pick-up sound from script

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     public InventoryItem item;
-    public AudioClip pickupSound; // pick-up sound
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,14 +17,6 @@ public class ItemPickup : MonoBehaviour
                 if (questManager != null)
                 {
                     questManager.CollectScript(); // reminding quest manager to show correct number of scripts collected
-                }
-                if (pickupSound != null)
-                {
-                    GameObject audioObject = new GameObject("PickupSound");
-                    AudioSource audioSource = audioObject.AddComponent<AudioSource>();
-                    audioSource.clip = pickupSound;
-                    audioSource.Play();
-                    Destroy(audioObject, pickupSound.length); //destroy audio object
                 }
                 Destroy(gameObject); //destroy item upon pick-up
             }
